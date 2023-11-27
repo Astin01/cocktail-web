a1fb508e5780783a8d49c2cb0a388a0ce83bc515
Donghun Won <wonhun1225@gmail.com>
2023-11-27 23:24:30 +0900

    feat : swagger 추가

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,5 +1,28 @@
+declare const module: any;
+
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CockTail API')
+    .setDescription('한국식 칵테일 정보 API')
+    .setVersion('1.0')
+    .addTag('cocktail')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`listening on port ${port}`);
 
   if (module.hot) {
     module.hot.accept();
     module.hot.dispose(() => app.close());
   }
+}
+bootstrap();
