9eb13b5db0a87c946d9db4f02a57f55c57240d9f
Donghun Won <wonhun1225@gmail.com>
2023-11-30 09:28:52 +0900

    feat : 로그인 유저 정보 요청

---
+++ front/src/utils/getUserInfo.js
@@ -0,0 +1,10 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+export const getUserInfo = createAsyncThunk('userdata', async () => {
+  try {
+    const response = await axios.get('/getinfo');
+    return response.data.user;
+  } catch (error) {
+    console.error(error);
+  }
+});
